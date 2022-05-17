0fd737bbb4428e0d74cd21e0adf097da2fe11199
zhoutk <zhoutk@189.cn>
2022-05-17 15:00:35 +0800

    check_type

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include "zjson.hpp"
+#include "check_type.hpp"
+#include <array>
+#include <vector>
+
+using namespace ZJSON;
 
 int main(int argc, char* argv[])
 {
-	std::cout << "---------------------------- Hell World! ----------------------------------" << std::endl;
+	/*Json ajson;
+	ajson.AddValueString("name", "kevin");
+	ajson.AddValue(Type::Number, "age", 18);
+	std::cout << ajson.toString() << std::endl;*/
+
+	bool pp;
+	int pp1;
+	long pp2;
+	long long pp3;
+	float pp4;
+	double pp5;
+	string pp6;
+	std::vector<int>pp7;
+	std::array<int,8>pp8;
+
+	std::cout << GetVarTypeName(pp) << std::endl;
+	std::cout << GetVarTypeName(pp1) << std::endl;
+	std::cout << GetVarTypeName(pp2) << std::endl;
+	std::cout << GetVarTypeName(pp3) << std::endl;
+	std::cout << GetVarTypeName(pp4) << std::endl;
+	std::cout << GetVarTypeName(pp5) << std::endl;
+	std::cout << GetVarTypeName(pp6) << std::endl;
+	std::cout << GetVarTypeName(pp7) << std::endl;
+	std::cout << GetVarTypeName(pp8) << std::endl;
+
+	system("pause");
 }
---
+++ src/zjson.hpp
@@ -0,0 +1,115 @@
+﻿#pragma once
+#include <string>
+
+namespace ZJSON {
+
+	static const int max_depth = 100;
+
+    using std::string;
+    using std::move;
+
+	enum class Type {
+		Error,
+		False,
+		True,
+		Null,
+		Number,
+		String,
+		Array,
+		Object,
+		Raw
+	};
+
+	class Json {
+		Json *next;
+		Json *prev;
+		Json *child;
+		Type type;
+		string data;
+		string name;
+	public:
+		Json() {
+			this->next = nullptr;
+			this->prev = nullptr;
+			this->child = nullptr;
+			this->name = "";
+			this->type = Type::Object;
+		}
+
+        Json(Type type) : Json(){
+			this->type = type;
+        }
+
+		Json(Type type, string value) : Json(type) {
+            this->data = value;
+		}
+
+		template<typename T> void AddValue(Type type, string name, T value) {
+			Json* node = new Json(type);
+			node->name = name;
+			switch (type)
+			{
+			case ZJSON::Type::Error:
+				break;
+			case ZJSON::Type::False:
+				node->data = "false";
+				break;
+			case ZJSON::Type::True:
+				node->data = "true";
+				break;
+			case ZJSON::Type::Null:
+				node->data = "null";
+				break;
+			case ZJSON::Type::Number:
+				node->data = std::to_string(value);
+				break;
+			case ZJSON::Type::String:
+				node->data = value;
+				break;
+			case ZJSON::Type::Array:
+				break;
+			case ZJSON::Type::Object:
+				break;
+			case ZJSON::Type::Raw:
+				break;
+			default:
+				break;
+			}
+		}
+
+		bool AddValueString(string name, string value) {
+			if (this->type == Type::Object || this->type == Type::Array) {
+				Json* node = new Json(Type::String, value);
+				node->name = name;
+				this->child = node;
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		void toString(Json json, string & result) {
+			if (json.type == Type::Object) {
+				result.append("{");
+				if (json.child)
+					toString(*this->child, result);
+					result += "}";
+			}
+			else if (json.type == Type::String) {
+				result += "\"" + json.name + "\":\"" + json.data + "\"";
+			}
+			else if (json.type == Type::Number) {
+				result += "\"" + json.name + "\":" + json.data;
+			}
+		}
+
+		string toString() {
+			string result;
+			this->toString(*this, result);
+			return result;
+		}
+
+	};
+
+}
