2794ecc32897d3e5126ae9d8389075d8beb7c150
zhoutk <zhoutk@189.cn>
2022-05-18 16:41:35 +0800

    add test item.

--- tests/test_object.cpp
+++ tests/test_object.cpp
@@ -16,10 +16,12 @@ TEST(TestObject, test_object_1) {
 	EXPECT_EQ(ajson.AddValueBase("longlong", ll), true);
 	EXPECT_EQ(ajson.AddValueBase("sex", true), true);
 	EXPECT_EQ(ajson.AddValueBase("name", data), true);
-	EXPECT_EQ(ajson.AddValueBase("school-cn", "第八十五中学"), true);
+	EXPECT_EQ(ajson.AddValueBase("school-cn", "绗叓鍗佷簲涓"), true);
 	EXPECT_EQ(ajson.AddValueBase("school-en", "the 85th."), true);
 	EXPECT_EQ(ajson.AddValueBase("age", 10), true);
 	EXPECT_EQ(ajson.AddValueBase("scores", 95.98), true);
 	EXPECT_EQ(ajson.AddValueBase("classroom", f), true);
 	EXPECT_EQ(ajson.AddValueBase("index", '6'), true);
+
+	EXPECT_EQ(ajson.toString(), "{ \"long\":123, \"longlong\" : 56789, \"sex\" : true, \"name\" : \"kevin\", \"school-cn\" : \"绗叓鍗佷簲涓\", \"school-en\" : \"the 85th.\", \"age\" : 10, \"scores\" : 95.98, \"classroom\" : 9.012345, \"index\" : 54 }");
 }
